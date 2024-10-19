67df42a2d299d2572962351e8b3a7bfdc91bfa8f
DIEUHA <sunidieu@gmail.com>
2024-10-19 13:09:25 +0700

    init source

---
+++ Sample.py
@@ -0,0 +1,18 @@
+full_name = "Ha thi thu dieu"
+first_name = "Dieu"
+last_name = "Ha"
+age = "32"
+is_musician = True 
+#Strings 
+print(f"Hello {first_name}")
+#Interger 
+print(f"Your age is {age}" )
+#Boolean 
+print(f"Your job is {is_musician}")
+if is_musician: 
+    print("You will be success") 
+else: 
+    print("You will not be success")
+#Typecasting - Chuyển loại data này sang loại data khác 
+age = float(age) 
+print (age)
